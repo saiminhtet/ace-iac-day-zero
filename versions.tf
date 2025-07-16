7b9457a0920ecd2564df4fa9b717f23384643e72
saiminhtet <htetsaimin@gmail.com>
2025-07-16 19:25:32 +0800

    updated new version

--- versions.tf
+++ versions.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "~> 3.1.5"
+      version = "~> 3.0.1"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 4.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.80.0"
+      version = "~> 3.0.0"
     }
   }
   required_version = ">= 1.0"
