6efd5f2553a2778f7815df6b8cb49d37b7de95da
saiminhtet <htetsaimin@gmail.com>
2025-07-16 23:22:13 +0800

    updated new version

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "~> 2.24.4"
+      version = "~> 3.0.1"
     }
     aws = {
       source  = "hashicorp/aws"
